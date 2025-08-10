b1d3d6a200b3034cc5ea6cb101fd8d2a875f3fdf
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-08-10 20:01:56 +0200

    Minor

--- src/main/kotlin/studio/vitr/seed/model/User.kt
+++ src/main/kotlin/studio/vitr/seed/model/User.kt
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import studio.vitr.seed.constants.Properties.ID
+import studio.vitr.seed.constants.Properties.USER_ID
 import studio.vitr.seed.errors.MissingExpectedParameter
 import studio.vitr.seed.utils.TimeUtil
 import java.util.*
@@ -18,5 +18,5 @@ data class User(
     val password: String,
     val createdAt: Long = TimeUtil.now(),
 ) {
-    fun idStr() = id?.toString() ?: throw MissingExpectedParameter(ID)
+    fun idStr() = id?.toString() ?: throw MissingExpectedParameter(USER_ID)
 }
